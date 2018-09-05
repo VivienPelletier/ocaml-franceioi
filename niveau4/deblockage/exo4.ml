8b338c4743d5bde916755d3a10d9e901e5a88a3c
Vivien PELLETIER <vivien.pelletier@gmail.com>
2018-09-05 21:58:33 +0200

    exo4 ok, la version tableau en O(N*log(N)) est assez rapide

--- niveau4/deblockage/exo4.ml
+++ niveau4/deblockage/exo4.ml
@@ -4,56 +4,60 @@ let read_int = fun () ->
 let read_2_int = fun () ->
   Scanf.scanf " %d %d" (fun x y -> x, y)
 
+type coat_t = { 
+  min : int; 
+  max : int; 
+  mutable p1 : int; }
+
 let read_coats = fun () ->
   let nb_coat = read_int() in
-  let rec read_coats_rec = fun i acc ->
-    if i = 0 then
-      acc
-    else
+  let read_coat = fun i ->
       let min, max = read_2_int() in
-      read_coats_rec (i-1) ((min, max)::acc) in
-  read_coats_rec nb_coat []
+      {min=min; max=max; p1=0} in
+  Array.init nb_coat read_coat
 
-let compare_coat_by_min = fun (min1, max1) (min2, max2) ->
-  if min1 < min2 then
+let compare_coat_by_min = fun c1 c2 ->
+  if c1.min < c2.min then
     -1
-  else if min1 > min2 then
+  else if c1.min > c2.min then
     1
-  else if max1 > max2 then
+  else if c1.max > c2.max then
     -1
-  else if max1 < max2 then
+  else if c1.max < c2.max then
     1
   else
     0
 
-let compare_coat_by_max = fun (min1, max1, _) (min2, max2, _) ->
-  if max1 > max2 then
+let compare_coat_by_max = fun c1 c2 ->
+  if c1.max > c2.max then
     -1
-  else if max1 < max2 then
+  else if c1.max < c2.max then
     1
-  else if min1 < min2 then
+  else if c1.min < c2.min then
     -1
-  else if min1 > min2 then
+  else if c1.min > c2.min then
     1
   else
     0
 
 let compute_max_level = fun coats ->
-  let compute_position_mapi = fun i (min, max) ->
-    (min,max,i) in
-  let compute_level_mapi = fun p2 (_, _, p1) ->
-    List.length coats - p1 - p2 - 1 in
+  let compute_position_iteri = fun i coat ->
+    coat.p1 <- i in
+  let compute_level_mapi = fun p2 coat ->
+    Array.length coats - coat.p1 - p2 - 1 in
   let compute_max_fold = fun max_level curr_level ->
     if curr_level > max_level then
-      curr_level else
+      curr_level 
+    else
       max_level in
-  let coats = List.sort compare_coat_by_min coats in
-  let coats = List.mapi compute_position_mapi coats in
-  let coats = List.sort compare_coat_by_max coats in
-  let coats_levels = List.mapi compute_level_mapi coats in
-  match coats_levels with
-  | [] -> failwith "empty list"
-  | hd_level::tl -> List.fold_left compute_max_fold hd_level tl 
+  Array.sort compare_coat_by_min coats;
+  Array.iteri compute_position_iteri coats;
+  Array.stable_sort compare_coat_by_max coats;
+  let coats_levels = Array.mapi compute_level_mapi coats in
+  if Array.length coats_levels < 1 then
+    failwith "empty list"
+  else
+    Array.fold_left compute_max_fold coats_levels.(0) coats_levels 
 
 let _ = let coats = read_coats() in
   let max_level = compute_max_level coats in
