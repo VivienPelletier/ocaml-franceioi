88438af1fde152594d44b0d40557d587622d16e1
Vivien PELLETIER <vivien.pelletier@gmail.com>
2018-09-05 21:15:30 +0200

    version O(N*log(N)) mais pas assez rapide avec des listes (je n'ai pas trouvé l'algorithme en O(N*log(N)), j'ai du chercher sur internet)

--- niveau4/deblockage/exo4.ml
+++ niveau4/deblockage/exo4.ml
@@ -14,7 +14,7 @@ let read_coats = fun () ->
       read_coats_rec (i-1) ((min, max)::acc) in
   read_coats_rec nb_coat []
 
-let compare_coat = fun (min1, max1) (min2, max2) ->
+let compare_coat_by_min = fun (min1, max1) (min2, max2) ->
   if min1 < min2 then
     -1
   else if min1 > min2 then
@@ -26,45 +26,35 @@ let compare_coat = fun (min1, max1) (min2, max2) ->
   else
     0
 
-let is_include_coat = fun (min1, max1) (min2, max2, _) ->
-  (min1 >= min2 && max1 <= max2)
+let compare_coat_by_max = fun (min1, max1, _) (min2, max2, _) ->
+  if max1 > max2 then
+    -1
+  else if max1 < max2 then
+    1
+  else if min1 < min2 then
+    -1
+  else if min1 > min2 then
+    1
+  else
+    0
 
-let levelize_coats = fun coats ->
-  let rec merge_coat = fun (min1, max1) coats ->
-    match coats with
-    | [] -> []
-    | (min2,max2,nb2)::tl -> if min1 < min2 || max1 > max2 then
-        coats
-      else
-        (min2,max2,nb2+1) :: (merge_coat (min1, max1) tl) in
-  let rec levelize_coats_rec = fun coats levelized_coats ->
-    match coats with
-    | [] -> levelized_coats
-    | current_coat::tl -> match levelized_coats with
-      | [] -> let min, max = current_coat in
-        levelize_coats_rec tl [(min, max, 0)]
-      | other_coat::_ -> 
-        let new_levelized_coat =
-          if is_include_coat current_coat other_coat then
-            merge_coat current_coat levelized_coats
-          else
-            let min, max = current_coat in
-            (min,max,0) :: levelized_coats in
-        levelize_coats_rec tl new_levelized_coat in
-  let coats = List.sort compare_coat coats in
-  levelize_coats_rec coats []
-    
-let max_level_coats = fun coats ->
-  let max_level_coats_fold = fun max (_,_,x) ->
-    if max >= x then
-      max
-    else
-      x in
-  match coats with
+let compute_max_level = fun coats ->
+  let compute_position_mapi = fun i (min, max) ->
+    (min,max,i) in
+  let compute_level_mapi = fun p2 (_, _, p1) ->
+    List.length coats - p1 - p2 - 1 in
+  let compute_max_fold = fun max_level curr_level ->
+    if curr_level > max_level then
+      curr_level else
+      max_level in
+  let coats = List.sort compare_coat_by_min coats in
+  let coats = List.mapi compute_position_mapi coats in
+  let coats = List.sort compare_coat_by_max coats in
+  let coats_levels = List.mapi compute_level_mapi coats in
+  match coats_levels with
   | [] -> failwith "empty list"
-  | (_,_,nb)::tl -> 
-    List.fold_left max_level_coats_fold nb tl
+  | hd_level::tl -> List.fold_left compute_max_fold hd_level tl 
 
 let _ = let coats = read_coats() in
-  let coats = levelize_coats coats in
-  Printf.printf "%d\n" (max_level_coats coats)
+  let max_level = compute_max_level coats in
+  Printf.printf "%d\n" max_level
